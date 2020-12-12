1e2048ddbb610b8fbec4a4ac2b50ff6dab49cbfe
Christa Bridges <christa@pebblepad.co.uk>
2020-12-12 13:48:46 +0000

    refactor-front-end The input and output are now on the same page.

--- app.py
+++ app.py
@@ -10,72 +10,69 @@ from CurrentDateFinder import CurrentDateFinder
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", errorNumber = 0)
+    if request.method == "POST":
+        # CB 2020-10-19 Get the url from the url box
+        url = request.form.get("urlBox")
 
-@app.route("/submit")
-def submit():
-    # CB 2020-10-24 Initial variable values
-    author = None
-    title = None
-    publicationYear = None
-    response = None
-    soup = None
-
-    # CB 2020-10-19 Get the url from the url box
-    url = request.args.get("urlBox")
-    
-    # CB 2020-10-19 try to make a GET request to the URL
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except Exception:
-        return render_template("index.html", errorNumber=1)
-
-    websiteObjectRetriever = WebsiteObjectRetriever(soup)
-    deserializedJson = websiteObjectRetriever.retrieve()
-
-    # Find Author
-    try:
-        authorFinder = AuthorFinder(soup, deserializedJson)
-        author = authorFinder.find()
-
-    except Exception:
+        # CB 2020-10-24 Initial variable values
         author = None
-
-    # Find Title
-    try:
-        titleFinder = TitleFinder(soup)
-        title = titleFinder.find()
-
-    except Exception:
         title = None
-    
-
-
-    # Find Name of Website
-    try:
-        if author is None:
-            websiteNameFinder = WebsiteNameFinder(soup, deserializedJson)
-            author = websiteNameFinder.find()
-    except Exception:
-        author = None
-
-
-    # Find Year Of Publication
-    try:
-        publicationYearFinder = PublicationYearFinder(soup)
-        publicationYear = publicationYearFinder.find()
-    except Exception:
         publicationYear = None
-    
-    # Get Current Date
-    currentDateFinder = CurrentDateFinder()
-    currentDate = currentDateFinder.find()
-    
-    print(publicationYear)
+        response = None
+        soup = None
+        
+        # CB 2020-10-19 try to make a GET request to the URL
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+        
+        try:
+            response = requests.get(url, headers=headers)
+            soup = BeautifulSoup(response.text, "html.parser")
+        except Exception:
+            return render_template("index.html", errorNumber=1)
 
-    return render_template("output.html", url=url, author=author, title=title, publicationYear=publicationYear, currentDate=currentDate)
+        websiteObjectRetriever = WebsiteObjectRetriever(soup)
+        deserializedJson = websiteObjectRetriever.retrieve()
+
+        # Find Author
+        try:
+            authorFinder = AuthorFinder(soup, deserializedJson)
+            author = authorFinder.find()
+
+        except Exception:
+            author = None
+
+        # Find Title
+        try:
+            titleFinder = TitleFinder(soup)
+            title = titleFinder.find()
+
+        except Exception:
+            title = None
+        
+
+
+        # Find Name of Website
+        try:
+            if author is None:
+                websiteNameFinder = WebsiteNameFinder(soup, deserializedJson)
+                author = websiteNameFinder.find()
+        except Exception:
+            author = None
+
+
+        # Find Year Of Publication
+        try:
+            publicationYearFinder = PublicationYearFinder(soup)
+            publicationYear = publicationYearFinder.find()
+        except Exception:
+            publicationYear = None
+        
+        # Get Current Date
+        currentDateFinder = CurrentDateFinder()
+        currentDate = currentDateFinder.find()
+
+        return render_template("index.html", url=url, author=author, title=title, publicationYear=publicationYear, currentDate=currentDate)
+
+    return render_template("index.html", errorNumber = 0)
